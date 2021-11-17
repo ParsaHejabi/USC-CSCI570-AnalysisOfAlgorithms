fe55a5f1fe74a57ccb3aab1f51943a72088a49c0
Parsa Hejabi <parsa.hejabi@gmail.com>
2021-11-16 19:20:39 -0800

    Base

---
+++ main.py
@@ -0,0 +1,27 @@
+
+def genrate_input():  # Alireza
+    string1 = ""
+    string2 = ""
+    return string1, string2
+
+
+def write_output_file():  # Saujanya
+    pass
+
+
+'''
+Input: 
+    - X 
+    - Y
+'''
+
+
+def basic_solution(X, Y):
+
+    # def memory_efficient_solution():
+
+
+def profiler():
+    """
+    outputs CPU time
+    """
