c9a3c7c6aae628e2a8b0feebef19676d785d8967
Parsa Hejabi <parsa.hejabi@gmail.com>
2021-11-22 15:31:51 -0800

    Basic is done! without bugs!

--- 4082809473_1605039563_6426152619_basic.py
+++ 4082809473_1605039563_6426152619_basic.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-INPUT_FILE_ADDRESS = 'input1.txt'
+INPUT_FILE_ADDRESS = 'input3.txt'
 OUTPUT_FILE_ADDRESS = 'output.txt'
 ALPHA = dict({
     ('A', 'A'): 0,
@@ -64,17 +64,20 @@ def generate_string(base: str, indexes: list[int]) -> str:
 
 
 def basic_bottom_up(first_gene: str, second_gene: str):
-    opt_matrix = np.empty((len(first_gene), len(second_gene)))
-    for i in range(len(first_gene)):
-        opt_matrix[i, 0] = i
+    """
+    O(len(first_gene) x len(second_gene))
+    """
+    opt_matrix = np.empty((len(first_gene) + 1, len(second_gene) + 1))
+    for i in range(len(first_gene) + 1):
+        opt_matrix[i, 0] = i * DELTA
 
-    for j in range(len(second_gene)):
-        opt_matrix[0, j] = j
+    for j in range(len(second_gene) + 1):
+        opt_matrix[0, j] = j * DELTA
 
-    for j in range(1, len(second_gene)):
-        for i in range(1, len(first_gene)):
-            first_gene_char = first_gene[i]
-            second_gene_char = second_gene[j]
+    for j in range(1, len(second_gene) + 1):
+        for i in range(1, len(first_gene) + 1):
+            first_gene_char = first_gene[i - 1]
+            second_gene_char = second_gene[j - 1]
             mismatch = opt_matrix[i - 1, j - 1] + \
                 ALPHA[(first_gene_char, second_gene_char)]
             skip_first_gene = opt_matrix[i - 1, j] + DELTA
@@ -85,31 +88,44 @@ def basic_bottom_up(first_gene: str, second_gene: str):
 
 
 def basic_top_bottom(opt_matrix, first_gene, second_gene):
-    i = len(first_gene) - 1
-    j = len(second_gene) - 1
+    """
+    opt_matrix[i, j] = optimal matched sequence from indexes 0 to i in the first gene 
+        and optimal matched seqeunce from indexes 0 to j in the second gene
+    """
+    i = len(first_gene)
+    j = len(second_gene)
     first_matched_sequence = ''
     second_matched_sequence = ''
     while i != 0 and j != 0:
         if opt_matrix[i, j] == opt_matrix[i - 1, j] + DELTA:
-            first_matched_sequence += "_"
+            first_matched_sequence += first_gene[i - 1]
+            second_matched_sequence += "_"
             i -= 1
         elif opt_matrix[i, j] == opt_matrix[i, j - 1] + DELTA:
-            second_matched_sequence += "_"
+            first_matched_sequence += "_"
+            second_matched_sequence += second_gene[j - 1]
             j -= 1
         else:
-            first_matched_sequence += first_gene[i]
-            second_matched_sequence += second_gene[j]
+            first_matched_sequence += first_gene[i - 1]
+            second_matched_sequence += second_gene[j - 1]
             i -= 1
             j -= 1
 
-    if i == 0:
-        first_matched_sequence += first_gene[i]
-        second_matched_sequence += second_gene[:j + 1][::-1]
-    elif j == 0:
-        first_matched_sequence += first_gene[:i + 1][::-1]
-        second_matched_sequence += second_gene[j]
+    if j == 0:
+        second_matched_sequence = second_matched_sequence[::-1]
+        second_matched_sequence = '_' * \
+            len(first_gene[0:i]) + second_matched_sequence
+        first_matched_sequence = first_matched_sequence[::-1]
+        first_matched_sequence = first_gene[0:i] + first_matched_sequence
 
-    return first_matched_sequence[::-1], second_matched_sequence[::-1], opt_matrix[-1, -1]
+    elif i == 0:
+        first_matched_sequence = first_matched_sequence[::-1]
+        first_matched_sequence = '_' * \
+            len(second_gene[:j]) + first_matched_sequence
+        second_matched_sequence = second_matched_sequence[::-1]
+        second_matched_sequence = second_gene[0:j] + second_matched_sequence
+
+    return first_matched_sequence, second_matched_sequence, opt_matrix[-1, -1]
 
 
 if __name__ == '__main__':
@@ -118,28 +134,24 @@ if __name__ == '__main__':
     first_base_string_indices = read_input_file_dict['first_base_string_indices']
     second_base_string = read_input_file_dict['second_base_string']
     second_base_string_indices = read_input_file_dict['second_base_string_indices']
-    # print(first_base_string, first_base_string_indices,
-    #       second_base_string, second_base_string_indices)
     first_gene = generate_string(first_base_string, first_base_string_indices)
     second_gene = generate_string(
         second_base_string, second_base_string_indices)
-    # print(first_gene, second_gene)
-    # assert first_gene == 'ACACTGACTACTGACTGGTGACTACTGACTGG', "Houston we have a problem in generating strings!"
-    # assert second_gene == 'TATTATACGCTATTATACGCGACGCGGACGCG', "Houston we have a problem in generating strings!"
 
     opt_matrix = basic_bottom_up(first_gene, second_gene)
+
     # print(opt_matrix)
     first_matched_sequence, second_matched_sequence, score = basic_top_bottom(
         opt_matrix, first_gene, second_gene)
     print(first_gene)
     print(second_gene)
-    # print(len(first_matched_sequence), len(second_matched_sequence))
-    # print(first_matched_sequence, second_matched_sequence, score)
-    print(score)
     print(first_matched_sequence)
     print(second_matched_sequence)
-    assert first_matched_sequence[:50] == 'ACACACTGAC_TACTGACTGGTG_ACTACTG_ACT_G_GACTGAC_TACT'
-    assert second_matched_sequence[:50] == 'TAT_TA_TTATACG_CTA_TTATACG_CGACGCGGACGC_G_T_ATACG_'
+    print(score)
+    assert len(first_matched_sequence) == len(second_matched_sequence)
 
-    assert first_matched_sequence[-51:] == 'CTACTG_ACT_G_GACTGAC_TACTGACTGGTG_ACTACTG_ACT_G_G_'
-    assert second_matched_sequence[-51:] == 'G_CGACGCGGACGC_G_T_ATACG_CTA_TTATACG_CGACGCGGACGCG'
+    # assert first_matched_sequence[:50] == 'ACACACTGAC_TACTGACTGGTG_ACTACTG_ACT_G_GACTGAC_TACT'
+    # assert second_matched_sequence[:50] == 'TAT_TA_TTATACG_CTA_TTATACG_CGACGCGGACGC_G_T_ATACG_'
+
+    # assert first_matched_sequence[-51:] == 'CTACTG_ACT_G_GACTGAC_TACTGACTGGTG_ACTACTG_ACT_G_G_'
+    # assert second_matched_sequence[-51:] == 'G_CGACGCGGACGC_G_T_ATACG_CTA_TTATACG_CGACGCGGACGCG'
